b3413b9e9f8dfa29ef53f42b7debef00a980a4d1
schra_000 <schra_000@10.30.9.69>
2014-11-12 16:02:15 -0600

    2nd commit

--- src/randommessagegenerator/RandomMessageGenerator.java
+++ src/randommessagegenerator/RandomMessageGenerator.java
@@ -1,5 +1,6 @@
-
 package randommessagegenerator;
+
+import java.util.*;
 
 /**
  *
@@ -7,6 +8,66 @@ package randommessagegenerator;
  */
 public class RandomMessageGenerator {
 
-    
+    private List<String> randomMessageList;
+
+    public RandomMessageGenerator() {
+        randomMessageList = new ArrayList<>();
+    }
+
+    public void displayList() {
+        for (String s : randomMessageList) {
+            System.out.println(s);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.randomMessageList);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RandomMessageGenerator other = (RandomMessageGenerator) obj;
+        if (!Objects.equals(this.randomMessageList, other.randomMessageList)) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getRandomMessage() {
+        Random rnd = new Random(System.nanoTime());
+        int num = rnd.nextInt(randomMessageList.size());
+        return randomMessageList.get(num);
+    }
+
+    public void removeDupes() {
+        Set<String> dupes = new HashSet<>(randomMessageList);
+        randomMessageList = new ArrayList<>(dupes);
+    }
+
+    public void addMessage(String message) {
+        randomMessageList.add(message);
+        removeDupes();
+    }
+
+    public void removeMessage(String message) {
+        randomMessageList.remove(message);
+    }
+
+    public List<String> getRandomMessageList() {
+        return randomMessageList;
+    }
+
+    public void setRandomMessageList(List<String> randomMessageList) {
+        this.randomMessageList = randomMessageList;
+    }
     
 }
--- src/randommessagegenerator/Startup.java
+++ src/randommessagegenerator/Startup.java
@@ -8,5 +8,37 @@ public class Startup {
     
     public static void main(String[] args) {
         
+        RandomMessageGenerator rmg = new RandomMessageGenerator();
+                
+        rmg.addMessage("Beer");
+        rmg.addMessage("Wine");
+        rmg.addMessage("Liquor");
+        rmg.addMessage("Coffee");
+        rmg.addMessage("Water");
+        rmg.addMessage("Milk");
+        rmg.addMessage("Tea");
+        rmg.addMessage("Coffee");
+        rmg.addMessage("Beer");
+        rmg.addMessage("Wine");
+        rmg.addMessage("Liquor");
+        rmg.addMessage("Coffee");
+        rmg.addMessage("Water");
+        rmg.addMessage("Milk");
+        rmg.addMessage("Tea");
+        rmg.addMessage("Coffee");
+        
+        System.out.println("List with Duplicates:");
+        rmg.displayList();
+        
+        rmg.removeDupes();
+        System.out.println("List without duDuplicatespes:");
+        rmg.displayList();
+        
+        System.out.println("Random message:");
+        System.out.println(rmg.getRandomMessage());
+        
+        rmg.removeMessage("Dog");
+        rmg.displayList();
+        
     }
 }
